780d220e5cc93384ea7dbe53faa6293c2f53ffe0
Keita Hayashi <keitahayashi@Keitas-MacBook-Air.local>
2021-07-05 22:29:06 +0900

    slice

--- main/hello.go
+++ main/hello.go
@@ -5,8 +5,51 @@ import (
 )
 
 func main() {
-	a := []int{10, 20, 30, 40, 50, 60}
-	b := a[1:3]
+	a := []int{10, 20, 30}
 	fmt.Println(a)
-	fmt.Println(b)
+
+	a = push(a, 1000)
+	fmt.Println(a)
+
+	a = pop(a)
+	fmt.Println(a)
+
+	a = unshift(a, 1000)
+	fmt.Println(a)
+
+	a = shift(a)
+	fmt.Println(a)
+
+	a = insert(a, 1000, 2)
+	fmt.Println(a)
+
+	a = remove(a, 2)
+	fmt.Println(a)
+}
+
+func push(a []int, v int) []int {
+	return append(a, v)
+}
+
+func pop(a []int) []int {
+	return a[:len(a)-1]
+}
+
+func unshift(a []int, v int) []int {
+	return append([]int{v}, a...)
+}
+
+func shift(a []int) []int {
+	return a[1:]
+}
+
+func insert(a []int, v int, p int) []int {
+	a = append(a, 0)
+	a = append(a[:p+1], a[p:len(a)-1]...)
+	a[p] = v
+	return a
+}
+
+func remove(a []int, p int) []int {
+	return append(a[:p], a[p+1:]...)
 }
