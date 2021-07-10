931aee6eee8cb8c781dba74bebaf2ac2fa755598
keitha13 <keitha9452@gmail.com>
2021-07-10 14:04:25 +0900

    pointer

--- main/hello.go
+++ main/hello.go
@@ -5,20 +5,9 @@ import (
 )
 
 func main() {
-	data := "*新しい値*"
-	m1 := modify(data)
-	data = "+new data+"
-	m2 := modify(data)
-
-	fmt.Println(m1())
-	fmt.Println(m2())
-}
-
-func modify(d string) func() []string {
-	m := []string{
-		"1st", "2nd",
-	}
-	return func() []string {
-		return append(m, d)
-	}
+	n := 123
+	p := &n
+	fmt.Println("number:", n)
+	fmt.Println("pointer:", p)
+	fmt.Println("value:", *p)
 }
