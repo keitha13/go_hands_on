77b26408aadeec431e7235bddfdb99209799f91a
keitha13 <keitha9452@gmail.com>
2021-07-11 21:32:49 +0900

    jadge prime num

--- main/hello.go
+++ main/hello.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"fmt"
+	"hello"
+	"strconv"
 )
 
-type Mydata struct {
-	Name string
-	Data []int
+type intp int
+
+func (num intp) IsPrime() bool {
+	n := int(num)
+	for i := 2; i <= (n / 2); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
-func (md Mydata) PrintData() {
-	fmt.Println("*** Mydata ***")
-	fmt.Println("Name: ", md.Name)
-	fmt.Println("Data: ", md.Data)
-	fmt.Println("*** end ***")
+func (num intp) PrimeFactor() []int {
+	ar := []int{}
+	x := int(num)
+	n := 2
+	for x > n {
+		if x%n == 0 {
+			x /= n
+			ar = append(ar, n)
+		} else {
+			if n == 2 {
+				n++
+			} else {
+				n += 2
+			}
+		}
+	}
+	ar = append(ar, x)
+	return ar
 }
 
 func main() {
-	taro := Mydata{
-		"hanako", []int{98, 76, 54, 32, 10},
-	}
-	taro.PrintData()
+	s := hello.Input("type a number")
+	n, _ := strconv.Atoi(s)
+	x := intp(n)
+	fmt.Printf("%d [%t]. \n", x, x.IsPrime())
+	fmt.Println(x.PrimeFactor())
+	x *= 2
+	x++
+	fmt.Printf("%d [%t]. \n", x, x.IsPrime())
+	fmt.Println(x.PrimeFactor())
 }
