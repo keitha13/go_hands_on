4793e24307189d16a1950f59d22fe6ab5f025bf8
keitha13 <keitha9452@gmail.com>
2021-07-19 20:38:54 +0900

    select

--- main/hello.go
+++ main/hello.go
@@ -2,33 +2,35 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 )
 
-func prmsg(n int, s string) {
-	fmt.Println(s)
-	time.Sleep(time.Duration(n) * time.Millisecond)
-}
-
-func first(n int, c chan string) {
-	const nm string = "first-"
-	for i := 0; i < 10; i++ {
-		s := nm + strconv.Itoa(i)
-		prmsg(n, s)
-		c <- s
-	}
-}
-
-func second(n int, c chan string) {
-	for i := 0; i < 10; i++ {
-		prmsg(n, "second:["+<-c+"]")
+func count(n int, s int, c chan int) {
+	for i := 1; i <= n; i++ {
+		c <- i
+		time.Sleep(time.Duration(s) * time.Millisecond)
 	}
 }
 
 func main() {
-	c := make(chan string)
-	go first(10, c)
-	second(10, c)
-	fmt.Println()
+	n1, n2, n3 := 3, 5, 10
+	m1, m2, m3 := 100, 75, 50
+	c1 := make(chan int)
+	go count(n1, m1, c1)
+	c2 := make(chan int)
+	go count(n2, m2, c2)
+	c3 := make(chan int)
+	go count(n3, m3, c3)
+
+	for i := 0; i < n1+n2+n3; i++ {
+		select {
+		case re := <-c1:
+			fmt.Println("* first ", re)
+		case re := <-c2:
+			fmt.Println("* second ", re)
+		case re := <-c3:
+			fmt.Println("* third ", re)
+		}
+	}
+	fmt.Println("*** finish. ***")
 }
