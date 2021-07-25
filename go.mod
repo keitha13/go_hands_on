4230c57e7a678c2afa27fd36b62eafbc5e78c98e
keitha13 <keitha9452@gmail.com>
2021-07-25 11:06:34 +0900

    sample fyne app

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module hello
 
 go 1.16
 
-require fyne.io/fyne/v2 v2.0.3
+require (
+	fyne.io/fyne v1.4.3
+	fyne.io/fyne/v2 v2.0.3
+)
--- main/hello.go
+++ main/hello.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"strconv"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
+	c := 0
 	a := app.New()
-
 	w := a.NewWindow("hello")
+	l := widget.NewLabel("hello fyne")
 	w.SetContent(
-		widget.NewLabel("hello Fyne!"),
+		widget.NewVBox(
+			l,
+			widget.NewButton("click me", func() {
+				c++
+				l.SetText("count: ", +strconv.Itoa(c))
+			}),
+		),
 	)
-
 	w.ShowAndRun()
 }
